9ea66a0174505c18a48fe5605eceeb76ce4de192
Gabriel Hervatin <gvghervatin@gmail.com>
2024-05-04 14:58:51 -0300

    major changes

--- Ing_finder_exec.py
+++ Ing_finder_exec.py
@@ -5,18 +5,13 @@ model_path = 'models/ingredient_ner_2'
 nlp = spacy.load(model_path)
 
 # New text to process
-text_to_process = ("Combine cabbage, onion, green pepper and sugar."", ""In saucepan, combine 1 tablespoon sugar"
-                   ", salt, celery seed, dry mustard, water, vinegar and oil."", ""Bring to a boil;"
-                   " pour over vegetables."", ""Cool in refrigerator for 24 hours."", ""Keeps for 3 weeks.")
+text_to_process = ("cabbage, onion, green pepper and sugar, sugar, salt, celery seed, mustard, water, vinegar and oil.")
+
+text_to_process = "1 Tbsp " + text_to_process
 
 text_to_process = (text_to_process
-                   .replace(', ', ', c. 1 Tbsp oz ')
-                   .replace('or ', 'c. 1 Tbsp oz ')
-                   .replace('and ', 'c. 1 Tbsp oz ')
-                   .replace('in ', 'c. 1 Tbsp oz ')
-                   .replace('In ', 'In c. 1 Tbsp oz ')
-                   .replace('whole', 'c. 1 Tbsp oz ')
-                   .replace('combine', 'c. 1 Tbsp oz ')
+                   .replace(',', ', 1 Tbsp ')
+                   .replace('and', ' 1 Tbsp ')
                    )
 
 # Process the text
