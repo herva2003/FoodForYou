2800c134f52f3f448dc329d733557f893f0a6b2d
Gabriel Hervatin <gvghervatin@gmail.com>
2024-06-01 12:42:33 -0300

    Code documentation

--- mongoDB/MongoDb.py
+++ mongoDB/MongoDb.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 client_mongo = MongoClient(
-    'mongodb+srv://gvghervatin:123456qwerty@cluster0.b4m0rm3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    'mongodb+srv://user:senha@cluster0.b4m0rm3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 
 db = client_mongo['PI5']
